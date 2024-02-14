0bc98de16e1c767679c3d85a744035eda3c88c26
jeeyeon <jeeyeon118@gmail.com>
2024-02-14 16:48:03 +0900

    수정반영_지연_240214

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import userInfoSlice from './userInfoSlice';
 
 import programSlice from "./programSlice"
+import empInfosSlice from "./empInfosSlice";
 
 const store = configureStore({
   reducer:{
     programSlice:programSlice.reducer,
-    userInfoSlice:userInfoSlice.reducer
+    userInfoSlice:userInfoSlice.reducer,
+    empInfos:empInfosSlice
   }
 });
 
