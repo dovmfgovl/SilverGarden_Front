4efc05217cbac5c859ec2b97a62be53036cb46d7
jeeyeon <jeeyeon118@gmail.com>
2024-02-14 12:40:42 +0900

    Plus_이지연_240214

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import empInfoSlice from "./empInfoSlice";
+import programSlice from "./programSlice"
+import empInfoSlice from "./empInfoSlice"
 
 const store = configureStore({
   reducer:{
+    programSlice:programSlice.reducer,
     empInfoSlice:empInfoSlice.reducer
   }
 })
