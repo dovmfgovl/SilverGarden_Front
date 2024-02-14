564947d7b2d377d248035964a463a7dc64e90322
teddy5025 <teddy5025@naver.com>
2024-02-14 17:31:54 +0900

    마이페이지 구상하기 태규 240214

--- src/pages/mypage/Mypage.jsx
+++ src/pages/mypage/Mypage.jsx
@@ -3,6 +3,12 @@ import React from 'react'
 const Mypage = () => {
   return (
     <div>Mypage</div>
+    // 서브라우팅은 필요없이 구상만
+      //admin emplist에 있는 대상을 쭉 나열한다(이걸 드랍다운으로 처리) 
+        // 이후 jwt가 완성되는대로 이 자리에 대체한다.
+        // 버튼 태그에 따라서 아래 나오는 페이지를 다르게 처리한다 -> 이건 서브라우팅 쓰면 될듯?
+        //
+    // empinfo의 포맷을 받는다. 
   )
 }
 
