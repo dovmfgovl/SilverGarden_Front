a8a854bc6a2d5b6cb3e340fc8038b50b562db36d
jeeyeon <jeeyeon118@gmail.com>
2024-02-22 15:03:30 +0900

    cal&dash&program_이지연_240222

--- src/pages/programcalendar/ProgramCalendar.jsx
+++ src/pages/programcalendar/ProgramCalendar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import CommonCalendar from '../../components/fullcalendar/CommonCalendar';
 
-const ProgramCalendar = () => {
+const ProgramCalendar = ({handleEvents}) => {
     const handleIndividualEventAdd = (event) => {
         console.log('Individual Calendar: Event Added', event);
         // 개별 캘린더에서 추가 이벤트 처리
@@ -18,31 +18,31 @@ const ProgramCalendar = () => {
     };
     //스프링 컨트롤러 url 입력(기본 CRUD)
     const commonUrls = {
-        listURL: 'program/pgList',
-        addURL: 'program/pgInsert',
-        updateURL: 'program/pgUpdate',
-        deleteURL: 'program/pgDelete',
+        listURL: 'calendar/list',
+        addURL: 'calendar/add',
+        updateURL: 'calendar/update',
+        deleteURL: 'calendar/delete',
     };
       // 개별 컴포넌트에서 사용할 데이터의 컬럼명 정의
     const columnNames = {
-        no: 'PG_NO',
-        title: 'PG_NAME',
-        start: 'PG_START',
-        end: 'PG_END',
+        no: 'PS_NO',
+        title: 'PS_NAME',
+        start: 'PS_START',
+        end: 'PS_END',
         color: 'COLOR', //카테고리별로 생성되는 색상
-        category: 'PG_CATEGORY', //색상을 구분하는 카테고리
-        content: 'PG_CONTENT'
+        category: 'PS_CATEGORY', //색상을 구분하는 카테고리
+        content: 'PS_INFO'
     };
 
     return (
         <div>
-            <h2>프로그램 캘린더</h2>
             <CommonCalendar
                 onEventAdd={handleIndividualEventAdd}
                 onEventUpdate={handleIndividualEventUpdate}
                 onEventDelete={handleIndividualEventDelete}
                 urls={commonUrls}
                 columnNames={columnNames}
+                handleEvents={handleEvents}
             />
         </div>
     );
