0423bfdf0788078a6efe20e88de0f85c22f9c447
teddy5025 <teddy5025@naver.com>
2024-02-26 11:11:17 +0900

    마이페이지 비밀번호 변겅 수정 240226

--- src/services/api/mypageApi.js
+++ src/services/api/mypageApi.js
@@ -15,3 +15,18 @@ export const callMypage = (params) => {
   });
 };
 
+export const changePw = (updatedPassword) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const response = axios({
+        method: "get",
+        url: process.env.REACT_APP_SPRING_IP + "my/changePassword",
+        params: updatedPassword,
+      });
+      resolve(response);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
