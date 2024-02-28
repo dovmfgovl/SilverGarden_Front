b44c084370cb2594bf308a206ae7eab2709176f5
977med <977med@naver.com>
2024-02-28 11:16:59 +0900

    결재필터구현_240228

--- src/pages/approval/ApprovalListHeader.jsx
+++ src/pages/approval/ApprovalListHeader.jsx
@@ -4,12 +4,41 @@ import { getApprovalDocCount } from '../../services/api/approvalApi';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
-const ApprovalListHeader = ({handleMenu, empData}) => {
+const ApprovalListHeader = ({handleMenu, empData, handleFilterdList}) => {
   const [docCount, setDocCount] = useState({
     deny_cnt: 0,
     wait_cnt: 0,
     progress_cnt: 0
   });
+
+  const [title, setTitle] = useState("제목");
+  const [gubun, setGubun] = useState("d_title")
+  const [keyword, setKeyword] = useState("")
+
+  const handleTitleSelect = (e)=>{
+    setTitle(e.target.innerText)
+    setGubun(e.target.id)
+  }
+
+  const handleEnter = (e) => {
+    if(e.key === 'Enter'){
+      handleFilterdList(gubun, keyword)
+    }
+    setKeyword("")
+  }
+  const handleKeyword = (e) =>{
+    setKeyword(e.target.value)
+  }
+
+  const handleSearch = (e) =>{
+    if(e.target.innerText === '전체목록'){
+      console.log("전체목록");
+      handleFilterdList()
+    }else{
+      handleFilterdList(gubun, keyword)
+    }
+    setKeyword("")
+  }
 
   const getDocCount = async () =>{
     const response = await getApprovalDocCount({e_no:empData.e_no})
@@ -28,16 +57,16 @@ const ApprovalListHeader = ({handleMenu, empData}) => {
       <FontAwesomeIcon className='mx-2' icon={faMagnifyingGlass} />
         <DropdownButton
           variant="secondary"
-          title="전체"
+          title={title}
           id="input-group-dropdown-1"
         >
-          <Dropdown.Item>전체</Dropdown.Item>
-          <Dropdown.Item id="n_title">제목</Dropdown.Item>
-          <Dropdown.Item id="n_content" >내용</Dropdown.Item>
-          <Dropdown.Item id="e_name" >작성자</Dropdown.Item>
+          <Dropdown.Item id="d_title" onClick={handleTitleSelect}>제목</Dropdown.Item>
+          <Dropdown.Item id="d_content" onClick={handleTitleSelect}>내용</Dropdown.Item>
+          <Dropdown.Item id="e_name" onClick={handleTitleSelect}>기안자</Dropdown.Item>
         </DropdownButton>
-        <Form.Control aria-label="Text input with dropdown button"/>
-        <Button variant="secondary">검색</Button>
+        <Form.Control aria-label="Text input with dropdown button" onKeyDown={handleEnter} onChange={handleKeyword} value={keyword}/>
+        <Button variant="secondary" onClick={handleSearch}>검색</Button>
+        <Button variant="secondary" className='ms-2' onClick={handleSearch}>전체목록</Button>
       </InputGroup>
     </div>
     <div>
