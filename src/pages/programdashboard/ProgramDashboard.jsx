f915f66891b8ffdf30b325a94149401af4a5bc07
jeeyeon <jeeyeon118@gmail.com>
2024-02-28 15:01:03 +0900

    update_이지연_240228

--- src/pages/programdashboard/ProgramDashboard.jsx
+++ src/pages/programdashboard/ProgramDashboard.jsx
@@ -8,21 +8,21 @@ import ProgramChart from './ProgramChart';
 const ProgramDashboard = ({programList, getProgramList}) => {
     const componentRef = useRef();
     return (
-        <>
+        <div className={styles.dashboardWrap}>
             <Print componentRef={componentRef}/>
             <div ref={componentRef} className="d-flex justify-content-center align-items-top vh-100">
-                <div className={styles.chartWrap}>
+                <div className={styles.chartWrap1}>
                         <ChartComponent programList={programList} getProgramList={getProgramList} />
                 </div>
-                <div className={styles.chartWrap}>
+                <div className={styles.chartWrap2}>
                     <ProgramChart programList={programList}/>
                 </div>
-                <div className={styles.chartWrap}>
+                <div className={styles.chartWrap3}>
                         <CustomShapeChartComponent />
                 </div>
             </div>
 
-        </>
+        </div>
     );
 };
 
