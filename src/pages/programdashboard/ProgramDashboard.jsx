cdda4487605ae424a2cb13bbc67d1672520bfd1d
jeeyeon <jeeyeon118@gmail.com>
2024-02-28 14:25:07 +0900

    reMerege_이지연_240228

--- src/pages/programdashboard/ProgramDashboard.jsx
+++ src/pages/programdashboard/ProgramDashboard.jsx
@@ -1,24 +1,39 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import ChartComponent from './ChartComponent';
 import styles from './programboard.module.css'
 import CustomShapeChartComponent from './CustomShapeChartComponent';
 import Print from '../../components/print/Print';
 import ProgramChart from './ProgramChart';
+import CommonCalendarLogic from '../../components/fullcalendar/CommonCalendarLogic';
 
-const ProgramDashboard = ({programList, getProgramList}) => {
+const ProgramDashboard = ({programList}) => {
     const componentRef = useRef();
+    const [pgCalList, setPgCalList] = useState([]);
+    useEffect(()=>{
+        const fetchData = async () => {
+            try {
+                const response = await CommonCalendarLogic.listDB('calendar/list');
+                setPgCalList(response); 
+            } catch (error) {
+                console.error('Error fetching member list:', error);
+            }
+        };
+        fetchData();
+        console.log(pgCalList);
+    },[])
+    
     return (
         <>
             <Print componentRef={componentRef}/>
             <div ref={componentRef} className="d-flex justify-content-center align-items-top vh-100">
                 <div className={styles.chartWrap}>
-                        <ChartComponent programList={programList} getProgramList={getProgramList} />
+                        <ChartComponent pgCalList={pgCalList}  />
                 </div>
                 <div className={styles.chartWrap}>
                     <ProgramChart programList={programList}/>
                 </div>
                 <div className={styles.chartWrap}>
-                        <CustomShapeChartComponent />
+                        <CustomShapeChartComponent pgCalList={pgCalList}/>
                 </div>
             </div>
 
